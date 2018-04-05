31fcaeaa10b348ca052620f2e405c824bdad9f01
nguyenky <lenguyenky1801@gmail.com>
2018-04-05 10:41:48 +0700

    component

---
+++ components/Uchiha.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import{
+  View,
+  Text
+} from "react-native";
+export default class Uchiha extends Component{
+  render(){
+    return(
+      <View>
+        <Text>
+          Component Uchiha
+        </Text>
+      </View>
+    );
+  }
+}
