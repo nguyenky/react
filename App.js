05d9268c5b9f40813b8e95132657a37a252fbc0b
nguyenky <lenguyenky1801@gmail.com>
2018-04-12 14:15:41 +0700

    image and get api

--- App.js
+++ App.js
@@ -9,50 +9,58 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TextInput
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      text:"hello",
+      text2:"hello2",
+      data:'le nguyen ky',
+    }
+    this.getApis();
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View>
+           <TextInput
+              style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+            />
+           <TextInput
+              style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={(text2) => this.setState({text2})}
+              value={this.state.text2}
+            />
+            <Text>{this.state.text}</Text>
+            <Text>{this.state.data}</Text>
       </View>
     );
   }
-}
+  getApis(){
+    fetch('https://facebook.github.io/react-native/movies.json', {
+      method: 'GET',
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);// your JSON response is here
+      console.log('----------------------');// your JSON response is here
+      this.state.data = responseJson;
+      console.log(this.state.data);// your JSON response is here
+        // alert(responseJson);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+        // alert(this.state.data);
+    })
+    .catch((error) => {
+        console.log(error);
+        alert('er');
+    });
+  }
+}
